ff597044af9c403cc4c0469b36f75f2ad4e3dfe6
xiaolan <xiaolan.wdj@taoboa.com>
2017-09-05 17:20:51 +0800

    change to ss

--- server.js
+++ server.js
@@ -1,106 +1,41 @@
-//  OpenShift sample Node application
-var express = require('express'),
-    fs      = require('fs'),
-    app     = express(),
-    morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
+const TCPRelay = require('./tcprelay').TCPRelay;
+const server = require('commander');
+const constants = require('./constants');
+const throng = require('throng');
+const log4js = require('log4js');
+const logger = log4js.getLogger('server');
 
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+server
+    .version(constants.VERSION)
+    .option('-m --method <method>', 'encryption method, default: aes-256-cfb')
+    .option('-k --password <password>', 'password')
+    .option('-s --server-address <address>', 'server address')
+    .option('-p --server-port <port>', 'server port, default: 8388')
+    .option('--log-level <level>', 'log level(debug|info|warn|error|fatal)', /^(debug|info|warn|error|fatal)$/i, 'info')
+    .option('--log-file <file>', 'log file')
+    .parse(process.argv);
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+throng({
+    workers: process.env.WEB_CONCURRENCY || 1,
+    master: startMaster,
+    start: startWorker
+});
 
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
-
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-
-  }
+function startMaster() {
+    logger.info('started master');
 }
-var db = null,
-    dbDetails = new Object();
 
-var initDb = function(callback) {
-  if (mongoURL == null) return;
+function startWorker(id) {
+    logger.info(`started worker ${id}`);
+    var relay = new TCPRelay({
+        serverAddress: process.env['SERVER_ADDRESS'] || server.serverAddress || '127.0.0.1',
+        serverPort: process.env['PORT'] || server.serverPort || 8388,
+        password: process.env['PASSWORD'] || server.password || 'shadowsocks-over-websocket',
+        method: process.env['METHOD'] || server.method || 'aes-256-cfb'
+    }, false);
 
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
-
-  mongodb.connect(mongoURL, function(err, conn) {
-    if (err) {
-      callback(err);
-      return;
-    }
-
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
-
-    console.log('Connected to MongoDB at: %s', mongoURL);
-  });
-};
-
-app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
-});
-
-app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
-});
-
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
-
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
-
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
-
-module.exports = app ;
+    relay.setLogLevel(server.logLevel);
+    relay.setLogFile(server.logFile);
+    relay.setServerName('server-' + id);
+    relay.bootstrap();
+}
